0cd92bc6f17ada6f15bf2dd9ccb7b1b3c3fb6936
Vishal Mishra <60419110+vishalaadee@users.noreply.github.com>
2022-06-06 20:32:57 +0530

    database

---
+++ dummy/model.py
@@ -0,0 +1,28 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+
+df=pd.read_csv(r'C:\Users\HP\Desktop\Trojan-Code\Training.csv')
+df.drop(df.columns[3:127],axis=1,inplace=True)
+train_df = df.drop('Unnamed: 133', axis=1)
+
+test_df=pd.read_csv(r'C:\Users\HP\Desktop\Trojan-Code\Testing.csv')
+test_df.drop(test_df.columns[3:127],axis=1,inplace=True)
+x=train_df.iloc[:,:-1]
+y=train_df.iloc[:,8]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+tree = DecisionTreeClassifier()
+tree.fit(x_train, y_train)
+input_data = [1,0,0,0,1,1,1,1]
+
+input_data_as_numpy_array= np.asarray(input_data)
+
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = tree.predict(input_data_reshaped)
+
+print(prediction)
